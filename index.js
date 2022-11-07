00ff07b1041ad8e5cba1bf920c205a0996f204f0
unknown <you@example.com>
2022-11-08 01:43:56 +0600

    Insert Services Data and Get Operation

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//Middle Wares
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2mjnncj.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run () {
+    try {
+        const serviceCollection = client.db('superkitch').collection('services')
+        
+        //Get All Services To Display Data
+        app.get('/services', async(req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(error => console.error(error))
+
+
+app.get('/', (req, res) => {
+    res.send("Superkitch Server is Running")
+})
+
+app.listen(port, () => {
+    console.log(`Superkitch Server running on ${port}`)
+})
