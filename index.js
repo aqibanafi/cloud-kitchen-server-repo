c7c734b08a820e4ebe858e267a0de6b2008d40df
unknown <you@example.com>
2022-11-10 02:12:26 +0600

    JWT Modify

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ async function run() {
         })
 
         //Delete Review
-        app.delete('/reviews/:id', verifyJWT, async (req, res) => {
+        app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await reviewCollection.deleteOne(query)
